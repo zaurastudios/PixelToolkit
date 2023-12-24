4d878e84f4d7d50df03317038e0a63d1bc9a4cf1
Zoclhas <zoclhas@gmail.com>
2023-12-24 17:05:01 +0400

    some drawer stuff

--- src/renderer/Screens/Home/projects/create-project.tsx
+++ src/renderer/Screens/Home/projects/create-project.tsx
@@ -1,21 +1,19 @@
 import * as Card from "@/components/ui/card";
 import { Plus } from "lucide-react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import * as Drawer from "@/components/ui/drawer";
 import { Button } from "@/components/ui/button";
-import * as Tabs from "@/components/ui/tabs";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { useState } from "react";
 import Projects from ".";
 
-type CreateInitSelect = "new" | "existing";
 interface OpenedDirProps {
   cancelled: boolean;
   filePaths: string[];
 }
 
 export default function CreateProject() {
-  const [initSelect, setInitSelect] = useState<CreateInitSelect>("new");
-
   const [path, setPath] = useState<string>("");
   const [cancelled, setCancelled] = useState(false);
 
@@ -38,7 +36,15 @@ export default function CreateProject() {
   });
 
   return (
-    <Drawer.Drawer shouldScaleBackground>
+    <Drawer.Drawer
+      onOpenChange={(e) => {
+        if (!e) {
+          setPath("");
+          setCancelled(false);
+        }
+      }}
+      shouldScaleBackground
+    >
       <Drawer.DrawerTrigger asChild>
         <MotionCardComponent
           whileTap={whileTap}
@@ -61,49 +67,59 @@ export default function CreateProject() {
         </Drawer.DrawerHeader>
 
         <div className="p-4">
-          <Tabs.Tabs
-            defaultValue="new"
-            onValueChange={(e) => setInitSelect(e as CreateInitSelect)}
-            className="w-[400px]"
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+            className="mt-2 flex flex-col gap-4"
           >
-            <Tabs.TabsList className="grid w-full grid-cols-2">
-              <Tabs.TabsTrigger value="new">Create New</Tabs.TabsTrigger>
-              <Tabs.TabsTrigger value="existing">
-                Select Existing
-              </Tabs.TabsTrigger>
-            </Tabs.TabsList>
-            <Tabs.TabsContent value="new">
-              <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-                <form className="mt-8 flex flex-col gap-4">
-                  <div className="flex items-center gap-4">
-                    <Button
-                      onClick={() => sendOpenDirReq()}
-                      className="w-max"
-                      variant={!!path && !cancelled ? "outline" : undefined}
-                    >
-                      Select Dir
-                    </Button>
-                    {cancelled ? (
-                      <p className="text-red-500 text-sm">
-                        Select a folder to continue
-                      </p>
-                    ) : (
-                      <code className="text-sm text-foreground/80">
-                        <em>{path}</em>
-                      </code>
-                    )}
-                  </div>
+            <div className="flex items-center gap-4">
+              <Button
+                onClick={() => sendOpenDirReq()}
+                className="w-max"
+                variant={!!path && !cancelled ? "outline" : undefined}
+              >
+                Select Dir
+              </Button>
+              {cancelled ? (
+                <p className="text-red-500 text-sm">
+                  Select a folder to continue
+                </p>
+              ) : (
+                <code id="dir-path" className="text-sm text-foreground/80">
+                  <em>{path}</em>
+                  <Input
+                    className="border-0 hidden p-0 max-w-none"
+                    disabled
+                    value={path}
+                    aria-hidden
+                  />
+                </code>
+              )}
+            </div>
 
-                  <div></div>
-                </form>
-              </motion.div>
-            </Tabs.TabsContent>
-            <Tabs.TabsContent value="existing">
-              <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-                Hiiii
-              </motion.div>
-            </Tabs.TabsContent>
-          </Tabs.Tabs>
+            {!!path && !cancelled && (
+              <AnimatePresence>
+                <motion.div
+                  key="show-once-path-is-selected"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  className="flex flex-col gap-4"
+                >
+                  <div className="grid w-full max-w-sm items-center gap-1.5">
+                    <Label htmlFor="project">Project Name</Label>
+                    <Input
+                      type="text"
+                      id="project"
+                      name="project"
+                      placeholder="My Amazing Project"
+                    />
+                  </div>
+                </motion.div>
+              </AnimatePresence>
+            )}
+          </form>
         </div>
       </Drawer.DrawerContent>
       <div />
