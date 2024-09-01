1db1fa25e35876fce344d25a14b92baded786f07
zoclhas <zoclhas@gmail.com>
2024-09-01 18:41:30 +0530

    Clean slate

--- src/renderer/components/project-config/tabs/color.tsx
+++
@@ -1,3 +0,0 @@
-export default function Color() {
-  return <div>color</div>;
-}
--- src/renderer/utils/title.ts
+++
@@ -1,6 +0,0 @@
-export default function titleString(str: string): string {
-  return str
-    .split(" ")
-    .map((s) => s[0].charAt(0).toUpperCase() + s.slice(1))
-    .join(" ");
-}
