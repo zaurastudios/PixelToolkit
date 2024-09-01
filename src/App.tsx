e30df42da28c390dc5436746c08e8f3d935ef335
zoclhas <zoclhas@gmail.com>
2024-09-01 19:51:28 +0530

    Theme set according to system

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./global.scss";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  useEffect(() => {
+    const setTheme = async () => {
+      const systemTheme = await getCurrentWindow().theme();
+      const storedTheme = localStorage.getItem("theme") as
+        | "light"
+        | "dark"
+        | null;
+      const theme = storedTheme || systemTheme || "dark";
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+      document.documentElement.classList.remove("light", "dark");
+      document.documentElement.classList.add(theme);
 
-  return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+      if (!storedTheme) {
+        localStorage.setItem("theme", theme);
+      }
+    };
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
-  );
+    setTheme();
+  }, []);
+  return <></>;
 }
 
 export default App;
