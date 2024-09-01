791e2ea27a06c75e68de9d8df0d13f48088a744a
zoclhas <zoclhas@gmail.com>
2024-09-01 19:07:11 +0530

    Init TW w/ shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+import "./global.scss";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
