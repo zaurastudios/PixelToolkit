edb031fe1cd6fb91e3bf8debc3d19618b468c38f
zoclhas <zoclhas@gmail.com>
2026-03-25 11:14:22 +0530

    feat: initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,10 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
-// Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
-// Create a new router instance
 const router = createRouter({ routeTree });
 
-// Register the router instance for type safety
 declare module "@tanstack/react-router" {
   interface Register {
     router: typeof router;
